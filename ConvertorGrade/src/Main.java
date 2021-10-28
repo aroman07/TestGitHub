26245f1f55a18ee4701e4f5cb8565e785f6415f8
aroman07 <87022612+aroman07@users.noreply.github.com>
2021-10-28 16:57:37 +0300

    Add files via upload

---
+++ ConvertorGrade/src/Main.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    private JPanel rootPanel;
+    private JTextArea txtCelsius;
+    private JTextField txtInputCelsius;
+    private JButton btnConvert;
+    private JTextArea txtOutputFahrenheit;
+
+
+    public Main() {
+        btnConvert.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnConvert.setBackground(Color.blue);
+                String grade = txtInputCelsius.getText();
+                txtOutputFahrenheit.setBackground(Color.red);
+                System.out.println(grade);
+                if (grade.equals("")){
+                    txtOutputFahrenheit.setText("Nu s-a introdus nimic");
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Convertor");
+
+        frame.setContentPane(new Main().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
